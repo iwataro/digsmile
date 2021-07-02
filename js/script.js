810d60fd305c87cf09364d5f22d862e02c96ea80
iwataro <62655263+iwataro@users.noreply.github.com>
2021-07-03 01:57:14 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,4 +5,12 @@ $(function(){
     $('.nav_toggle').on('click', function () {
         $('.nav_toggle, .nav').toggleClass('show');
     });
+
+});
+
+
+// スクロール変化
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("header-scroll", window.scrollY > 450);
 });
